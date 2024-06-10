8078ffa08be64048a47e20f51b4da9352fd66343
chiragaccolite <chiragarora@accolitedigital.com>
2024-06-10 11:09:40 +0530

    backend files updated

--- Backend/Client Submission Tracker (4)/Client Submission Tracker/Client-Submission-Tracker/src/main/java/com/accolite/ClientSubmissionTrackerApplication.java
+++ Backend/Client Submission Tracker (4)/Client Submission Tracker/Client-Submission-Tracker/src/main/java/com/accolite/ClientSubmissionTrackerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClientSubmissionTrackerApplication {
 
 	public static void main(String[] args) {
-			SpringApplication.run(ClientSubmissionTrackerApplication.class, args);
+		SpringApplication.run(ClientSubmissionTrackerApplication.class, args);
 	}
 
 }
