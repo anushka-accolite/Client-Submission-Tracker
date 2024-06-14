cff853e1b10264ca4fef2de635e8c61c2583998c
TARUN SAI <66516182+TARUNSAI090@users.noreply.github.com>
2024-06-14 11:56:55 +0530

    Add files via upload

--- FullLayout.js
+++ FullLayout.js
@@ -1,75 +1,3 @@
-// import React from 'react';
-// import { Outlet } from 'react-router-dom';
-// import Sidebar from './Sidebar';
-// import Header from './Header';
-// import { Container } from 'reactstrap';
-
-// const FullLayout = ({ showSidebarAndHeader }) => {
-//   return (
-//     <main>
-//       <div className="pageWrapper d-lg-flex">
-//         {/* Sidebar */}
-//         {showSidebarAndHeader && (
-//           <aside className="sidebarArea shadow" id="sidebarArea" style={{
-//             backgroundColor: "#052c4f",
-//             textShadow: "0px 0px 1px whitesmoke",
-//             fontFamily: 'Lato, sans-serif',
-//             boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.2)"
-//           }}>
-//             <Sidebar />
-//           </aside>
-//         )}
-
-//         {/* Content Area */}
-//         <div className="contentArea">
-//           {/* Header */}
-//           {showSidebarAndHeader && <Header />}
-          
-//           {/* Middle Content */}
-//           <Container className="p-4 wrapper" fluid>
-//             <Outlet />
-//           </Container>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default FullLayout;
-
-
-// import React from 'react';
-// import { Outlet } from 'react-router-dom';
-// import Sidebar from './Sidebar';
-// import Header from './Header';
-// import { Container } from 'reactstrap';
-
-// const FullLayout = ({ showSidebarAndHeader }) => {
-//   const [sidebarOpen, setSidebarOpen] = React.useState(true);
-
-//   const toggleSidebar = () => {
-//     setSidebarOpen(!sidebarOpen);
-//   };
-
-//   return (
-//     <div style={{ position: 'relative', overflowX: 'hidden' }}>
-//       <Header toggleSidebar={toggleSidebar} sidebarOpen={sidebarOpen} />
-//       <div style={{ display: 'flex' }}>
-//         {showSidebarAndHeader && (
-//           <Sidebar sidebarOpen={sidebarOpen} />
-//         )}
-//         <div style={{ flexGrow: 1 }}>
-//           <Container fluid>
-//             <Outlet />
-//           </Container>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FullLayout;
-
 
 import React from 'react';
 import { Outlet } from 'react-router-dom';
@@ -102,7 +30,7 @@ const FullLayout = ({ showSidebarAndHeader }) => {
           <Sidebar sidebarOpen={sidebarOpen} />
         )}
         <div className="content" style={{ flex: 1, overflow: 'auto' }}>
-          <Container fluid>
+          <Container fluid fixed>
             <Outlet />
           </Container>
         </div>
